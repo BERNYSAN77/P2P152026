ed18ba0378f5d707c85bb318eb48e01606eb49dc
Laboratorio <laboratorio@udla-ec.int>
2026-04-08 17:47:41 -0500

    Initial commit

---
+++ src/progreso1/ejermotor/interfaz/Main.java
@@ -0,0 +1,10 @@
+package progreso1.ejermotor.interfaz;
+
+
+import progreso1.ejermotor.negocio.Motor;
+
+public class Main {
+    public static void main(String[] args) {
+        Motor m1 = new Motor();
+    }
+}
---
+++ src/progreso1/ejermotor/negocio/Motor.java
@@ -0,0 +1,40 @@
+package progreso1.ejermotor.negocio;
+
+public class Motor {
+    private boolean estado;
+    private int velocidad;
+
+
+    public void encender(){
+        if(estado)
+            System.out.println("El motor ya se encuentra encendido");
+        else{
+            estado = true;
+            System.out.println("El motor se ha encendido");
+
+        }
+    }
+    public void apagar(){
+        if(estado){
+            estado = false;
+            velocidad = 0;
+            System.out.println("El motor ha sido apagado");}
+        else{
+            System.out.println("El motor se encuentra apagado");
+        }
+    }
+    public void ajustarVelocidad(int nuevaVelocidad){
+        if (nuevaVelocidad < 0 || nuevaVelocidad > 100){
+            System.out.println("El dato es erroneo");
+        }else{
+            velocidad = nuevaVelocidad;
+        }
+    }
+    public void mostrarDatos(){
+        if(estado){
+            System.out.println("El motor esta Encendido y la velocidad es: "+velocidad);
+
+        }
+        else System.out.println("El motor esta Apagado");
+    }
+}
